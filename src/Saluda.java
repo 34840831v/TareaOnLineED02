5a7e8f9b71e889077fb12f6f612ce5db6fa3e96f
Javier <Javier@LAPTOP-8Q3H0GFR>
2016-11-19 13:46:28 +0100

    Creamos la variablede clase_nombre

--- src/Saluda.java
+++ src/Saluda.java
@@ -4,9 +4,12 @@ public class Saluda {
 
 	public static void main(String[] args) {
 		Scanner teclado=new Scanner(System.in);
-		System.out.println("CUAL ES TU NOMBRE");
+		System.out.println("¿CUAL ES TU NOMBRE?");
 		String nombre=teclado.nextLine();
-		System.out.println(nombre);
-	}
+		System.out.println("HOLA " + nombre);
+		String clase_nombre;
+		clase_nombre=nombre;
+		System.out.println("El contenido de la variable clase_nombre es: " + clase_nombre);
+		}
 
 }
